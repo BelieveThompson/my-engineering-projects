5f7b22ccb6d1b8b252703ffd0584203ba1727000
BelieveThompson <thompsonbelieve417@gmail.com>
2026-09-24 09:11:37 +0100

    Create school.cpp

---
+++ school.cpp
@@ -0,0 +1,7 @@
+50%; transform: translateY(-50%)} [id="{{CONTAINER}}"] .a4a-bs-btn {-webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; overflow: hidden}à
+WHAT'S YOUR
+HOURLY PAY GOAL?
+â®• $12/H
+â®• $18/H
+â®• $25/H
+â®• $40/H
